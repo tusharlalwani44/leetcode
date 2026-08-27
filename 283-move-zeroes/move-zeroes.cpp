39ccb1e2a3547b3261eb0c01cedd8e2f456c5008
Tushar Lalwani <tusharlalwani44@gmail.com>
2026-08-28 00:11:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (82.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int position=0;
         int n=nums.size();
-        for(int i=0;i<n;i++){ // nums[]={0,1,0,2,12} --- i at 0
-        for(int j=i+1;j<n;j++){
-            if(nums[i]==0 && nums[j]!=0){
-                swap(nums[i],nums[j]);
-            } // 100312
-        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[position],nums[i]);
+                position++;
+            }
         }
     }
 };
